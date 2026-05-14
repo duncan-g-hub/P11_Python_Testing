39c2d0cdf2dcb1c9d447e4de3b5e7df6d73fc065
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-05-14 15:48:26 +0200

    add functional test_logout

---
+++ tests/functionals/test_logout.py
@@ -0,0 +1,21 @@
+import pytest
+import time
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def browser():
+    driver  = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+def test_user_can_logout(browser, live_server_url, valid_email):
+    browser.get(f"{live_server_url}/")
+    browser.find_element(By.NAME, "email").send_keys(valid_email)
+    browser.find_element(By.NAME, "email").submit()
+    time.sleep(1)
+    browser.find_element(By.LINK_TEXT, "Logout").click()
+    time.sleep(1)
+    assert "Registration Portal" in browser.page_source
