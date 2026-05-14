76c2774fc4c7098b3126e5b500154c02334efe80
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-05-14 11:28:40 +0200

    add test for logout

---
+++ tests/units/test_logout.py
@@ -0,0 +1,11 @@
+from tests.units.conftest import client
+
+def test_logout_should_redirect(client):
+    response = client.get('/logout')
+    assert response.status_code == 302
+
+def test_logout_should_redirect_to_index(client):
+    response = client.get('/logout', follow_redirects=True)
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert 'Registration Portal' in data
