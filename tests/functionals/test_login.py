732037d7b031b724cc6640348f6ae5bf51c54591
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-05-18 11:43:15 +0200

    refactor route names for test

--- tests/functionals/test_login.py
+++ tests/functionals/test_login.py
@@ -26,26 +26,3 @@ def test_user_cannot_login_with_wrong_email(browser, live_server_url, invalid_em
     browser.find_element(By.NAME, "email").submit()
     time.sleep(1)
     assert "You do not have access to booking." in browser.page_source
-
-# from tests.conftest import client, valid_email
-#
-# def test_user_can_login_and_see_dashboard(client, valid_email):
-#     response = client.get('/')
-#     assert response.status_code == 200
-#     assert 'Registration Portal' in response.data.decode()
-#
-#     response = client.post('/showSummary', data={'email': valid_email}, follow_redirects=True)
-#     data = response.data.decode()
-#
-#     assert response.status_code == 200
-#     assert 'Logout' in data
-#
-# def test_user_cannot_login_with_wrong_email(client, invalid_email="test@test.com"):
-#     response = client.get('/')
-#     assert response.status_code == 200
-#
-#     response = client.post('/showSummary', data={'email': invalid_email}, follow_redirects=True)
-#     data = response.data.decode()
-#
-#     assert response.status_code == 200
-#     assert "You do not have access to booking" in data
