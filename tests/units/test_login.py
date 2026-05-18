3168660ebbfb0987d7b7414bfceb4ff922142682
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-05-18 14:19:53 +0200

    add follow_redirects=True for tests with redirect

--- tests/units/test_login.py
+++ tests/units/test_login.py
@@ -8,12 +8,12 @@ def test_should_login_with_correct_email_and_status_code_200(client, valid_email
     assert 'Logout' in data
 
 def test_should_not_login_with_empty_email(client):
-    response = client.post('/show-summary', data={'email': ''})
+    response = client.post('/show-summary', data={'email': ''}, follow_redirects=True)
     data = response.data.decode()
-    assert "You do not have access to booking" in data
+    assert "You do not have access to booking." in data
 
 def test_should_not_login_with_wrong_email(client):
-    response = client.post('/show-summary', data={'email': "test@test.com"})
+    response = client.post('/show-summary', data={'email': "test@test.com"}, follow_redirects=True)
     data = response.data.decode()
     assert "You do not have access to booking" in data
 
