ab263bd1c379d9e21fb2afb2564efd2fef4db1ab
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-05-19 11:32:07 +0200

    add integration test for unmet condition in purchase_places

--- tests/intergrations/test_booking.py
+++ tests/intergrations/test_booking.py
@@ -1,5 +1,5 @@
 from tests.conftest import client, valid_club, valid_competition
-
+import html
 
 def test_integration_book_then_purchase_places(client, valid_club, valid_competition):
     competition_name = valid_competition["name"]
@@ -22,3 +22,23 @@ def test_integration_book_then_purchase_places(client, valid_club, valid_competi
 
     assert int(valid_competition["number_of_places"]) == places_before - int(number_of_place)
     assert int(valid_club["points"]) == points_before - int(number_of_place)
+
+def test_integration_book_then_purchase_places_with_too_many_places(client, valid_club, valid_competition):
+    competition_name = valid_competition["name"]
+    club_name = valid_club["name"]
+    places_before = int(valid_competition["number_of_places"])
+    points_before = int(valid_club["points"])
+    number_of_place = places_before + 1
+
+    response = client.get(f'/book/{competition_name}/{club_name}')
+    assert response.status_code == 200
+
+    response = client.post('/purchase-places', data={
+        'places': number_of_place,
+        'competition_name': competition_name,
+        'club_name': club_name
+    })
+    assert response.status_code == 200
+    assert "You can't book this number of places" in html.unescape(response.data.decode())
+    assert int(valid_competition["number_of_places"]) == places_before
+    assert int(valid_club["points"]) == points_before
