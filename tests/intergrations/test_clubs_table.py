4f1146a56f64919fde275d9fdab5a37f8ccf0642
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-05-20 09:41:10 +0200

    add integration test for clubs_table

---
+++ tests/intergrations/test_clubs_table.py
@@ -0,0 +1,21 @@
+from tests.conftest import client, valid_club, valid_competition
+
+
+def test_integration_purchase_places_then_clubs_table(client, valid_club, valid_competition):
+    competition_name = valid_competition["name"]
+    club_name = valid_club["name"]
+    points_before = int(valid_club["points"])
+    number_of_place = "4"
+
+    response = client.post('/purchase-places', data={
+        'places': number_of_place,
+        'competition_name': competition_name,
+        'club_name': club_name
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    assert 'Great-booking complete!' in response.data.decode()
+
+    response = client.get('/clubs-table')
+    assert response.status_code == 200
+    assert club_name in response.data.decode()
+    assert f"Points : {points_before - int(number_of_place)}" in response.data.decode()
